e6a4d752f409ee083505d51a26a5ce57859bf614
jvseki <jv.s.m1007@gmail.com>
2026-03-17 14:15:09 -0300

    change

--- app.py
+++ app.py
@@ -6,6 +6,15 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/barbearia')
+def barbearia():
+    return render_template('barbearia.html')
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
+
+app = app
+
 if __name__ == '__main__':
-    
     app.run(debug=True)
